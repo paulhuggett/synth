6c064d039cf203ddd63fa856d3d2f1c3a5725dcf
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-05-05 18:39:07 +0200

    Handle MIDI source connect and disconnect messages.

--- synth/tools/player_macos/AppDelegate.mm
+++ synth/tools/player_macos/AppDelegate.mm
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+#include <cinttypes>
+
 #import <AudioToolbox/AudioToolbox.h>
 
 #include "synth/voice_assigner.hpp"
@@ -44,6 +46,8 @@ static void stopAudio (AudioQueueRef queue) {
   NSLock *lock_;
 
   std::unique_ptr<synth::voice_assigner> voices_;
+  MIDIPortRef port_;
+  MIDIEndpointRef source_;
 }
 @end
 
@@ -60,6 +64,8 @@ static void stopAudio (AudioQueueRef queue) {
     lock_ = [NSLock new];
     voices_.reset (new synth::voice_assigner);
     //    osc_->set_frequency (synth::oscillator::frequency::fromfp (440.0));
+    port_ = 0;
+    source_ = 0;
   }
   return self;
 }
@@ -136,7 +142,7 @@ static void stopAudio (AudioQueueRef queue) {
         {
           unsigned const note = *(byte++);      // TODO: bit 7 must be 0.
           unsigned const velocity = *(byte++);  // TODO: bit 7 must be 0.
-          // NSLog (@"Note off chan=%u, note=%u, velocity=%u", chan, note, velocity);
+          NSLog (@"Note off chan=%u, note=%u, velocity=%u", chan, note, velocity);
           if ([self->lock_ lockBeforeDate:[NSDate dateWithTimeIntervalSinceNow:lockWaitTime]]) {
             self->voices_->note_off (note);
             [self->lock_ unlock];
@@ -148,10 +154,10 @@ static void stopAudio (AudioQueueRef queue) {
           unsigned const velocity = *(byte++);  // TODO: bit 7 must be 0.
           if ([self->lock_ lockBeforeDate:[NSDate dateWithTimeIntervalSinceNow:lockWaitTime]]) {
             if (velocity == 0) {
-              // NSLog (@"Note off chan=%u, note=%u, velocity=%u", chan, note, velocity);
+              NSLog (@"Note off chan=%u, note=%u, velocity=%u", chan, note, velocity);
               self->voices_->note_off (note);
             } else {
-              // NSLog (@"Note on chan=%u, note=%u, velocity=%u", chan, note, velocity);
+              NSLog (@"Note on chan=%u, note=%u, velocity=%u", chan, note, velocity);
               self->voices_->note_on (note);
             }
             [self->lock_ unlock];
@@ -194,10 +200,196 @@ static void stopAudio (AudioQueueRef queue) {
   }
 }
 
-static void ReadMIDIdevice (MIDIPacketList const *pktlist, void *userData, void *connRefCon) {
+// read MIDI
+// ~~~~~~~~~
+static void readMIDI (MIDIPacketList const *const packetList, void *userData, void *connRefCon) {
   if (AppDelegate *const ad = (__bridge AppDelegate *)connRefCon) {
-    [ad readMIDIPacketList:pktlist];
+    [ad readMIDIPacketList:packetList];
   }
+}
+
+static NSString *midiTypeName (MIDIObjectType type) {
+  switch (type) {
+    case kMIDIObjectType_Other:
+      return @"kMIDIObjectType_Other";
+    case kMIDIObjectType_Device:
+      return @"kMIDIObjectType_Device";
+    case kMIDIObjectType_Entity:
+      return @"kMIDIObjectType_Entity";
+    case kMIDIObjectType_Source:
+      return @"kMIDIObjectType_Source";
+    case kMIDIObjectType_Destination:
+      return @"kMIDIObjectType_Destination";
+
+    case kMIDIObjectType_ExternalDevice:
+      return @"kMIDIObjectType_ExternalDevice";
+    case kMIDIObjectType_ExternalEntity:
+      return @"kMIDIObjectType_ExternalEntity";
+    case kMIDIObjectType_ExternalSource:
+      return @"kMIDIObjectType_ExternalSource";
+    case kMIDIObjectType_ExternalDestination:
+      return @"kMIDIObjectType_ExternalDestination";
+  }
+  return @"unknown";
+}
+
+- (OSStatus)disconnectFromMIDISource {
+  OSStatus erc = ::MIDIPortDisconnectSource (self->port_, self->source_);
+  if (erc != noErr) {
+    return erc;
+  }
+  self->source_ = 0;
+  return noErr;
+}
+
+- (OSStatus)connectToMIDIUniqueID:(MIDIUniqueID)uniqueID {
+  OSStatus erc = 0;
+
+  MIDIEndpointRef endpoint = 0;
+  MIDIObjectType objectType = kMIDIObjectType_Other;
+  erc = ::MIDIObjectFindByUniqueID (uniqueID, &endpoint, &objectType);
+  if (erc != noErr) {
+    NSLog (@"reconnectToMIDIUniqueID.MIDIObjectFindByUniqueID: returned %d", erc);
+    return erc;
+  }
+  if (objectType != kMIDIObjectType_Source && objectType != kMIDIObjectType_ExternalSource) {
+    NSLog (@"reconnectToMIDIUniqueID wrong endpoint type");
+    return kMIDIWrongEndpointType;
+  }
+
+  if (self->source_ != 0) {
+    // Cleanly disconnect from our existing source if there is one.
+    erc = ::MIDIPortDisconnectSource (self->port_, self->source_);
+    self->source_ = 0;
+  }
+
+  CFStringRef sourceName = Nil;
+  erc = MIDIObjectGetStringProperty (endpoint, kMIDIPropertyName, &sourceName);
+  if (erc != noErr) {
+    NSLog (@"connectToMIDIUniqueID (child kMIDIPropertyName) failed %d", erc);
+  } else {
+    NSLog (@"Connecting to MIDI source '%@'", sourceName);
+    CFRelease (sourceName);
+    sourceName = Nil;
+  }
+
+  self->source_ = endpoint;
+  return ::MIDIPortConnectSource (self->port_, endpoint, (__bridge void *)self /*connRefCon*/);
+}
+
+- (OSStatus)midiObjectRemovedNotificaton:(MIDIObjectAddRemoveNotification const *)n {
+  OSStatus erc = noErr;
+  NSLog (@"MIDI object removed: parent=%" PRIu32 ", parentType=%@, child=%" PRIu32 ", childType=%@",
+         n->parent, midiTypeName (n->parentType), n->child, midiTypeName (n->childType));
+  if (n->childType == kMIDIObjectType_Source && n->child == self->source_) {
+    NSLog (@"Our MIDI source was removed!");
+    erc = [self disconnectFromMIDISource];
+  }
+  return erc;
+}
+
+- (OSStatus)midiObjectAddedNotification:(MIDIObjectAddRemoveNotification const *)n {
+  OSStatus erc = noErr;
+  NSLog (@"MIDI object added: parent=%" PRIu32 ", parentType=%@, child=%" PRIu32 ", childType=%@",
+         n->parent, midiTypeName (n->parentType), n->child, midiTypeName (n->childType));
+  if (n->childType == kMIDIObjectType_Source && self->source_ == 0) {
+    MIDIUniqueID uniqueID = 0;
+    erc = ::MIDIObjectGetIntegerProperty (n->child, kMIDIPropertyUniqueID, &uniqueID);
+    if (erc != noErr) {
+      NSLog (@"midiChangedNotification.MIDIObjectGetIntegerProperty: returned %d", erc);
+    } else {
+      erc = [self connectToMIDIUniqueID:uniqueID];
+    }
+  }
+  return erc;
+}
+
+- (void)midiChangedNotification:(MIDINotification const *)message {
+  switch (message->messageID) {
+    // Some aspect of the current MIDISetup has changed.  No data.  Should ignore this message if
+    // the other change notification messages are handled.
+    case kMIDIMsgSetupChanged:
+      break;
+    case kMIDIMsgObjectRemoved:
+      if (message->messageSize != sizeof (MIDIObjectAddRemoveNotification)) {
+        NSLog (@"MIDI Object removed: Message size was %u, expected %zu", message->messageSize,
+               sizeof (MIDIObjectAddRemoveNotification));
+      } else {
+        [self
+            midiObjectRemovedNotificaton:reinterpret_cast<MIDIObjectAddRemoveNotification const *> (
+                                             message)];
+      }
+      break;
+    case kMIDIMsgObjectAdded:
+      if (message->messageSize != sizeof (MIDIObjectAddRemoveNotification)) {
+        NSLog (@"MIDI Object Added: Message size was %u, expected %zu", message->messageSize,
+               sizeof (MIDIObjectAddRemoveNotification));
+      } else {
+        [self
+            midiObjectAddedNotification:reinterpret_cast<MIDIObjectAddRemoveNotification const *> (
+                                            message)];
+      }
+      break;
+    case kMIDIMsgPropertyChanged:
+      NSLog (@"MIDI Property Changed");
+      break;
+    case kMIDIMsgThruConnectionsChanged:
+      NSLog (@"MIDI Through Connections Changed");
+      break;
+    case kMIDIMsgSerialPortOwnerChanged:
+      NSLog (@"MIDI Serial Port Owner Changed");
+      break;
+    case kMIDIMsgIOError:
+      NSLog (@"MIDI IO Error");
+      break;
+    default:
+      NSLog (@"MIDI Change Notified (unknown message ID)");
+      break;
+  }
+}
+
+static void midiChanged (MIDINotification const *const message, void *refCon) {
+  if (AppDelegate *const ad = (__bridge AppDelegate *)refCon) {
+    [ad midiChangedNotification:message];
+  }
+}
+
+- (OSStatus)startMIDI {
+  MIDIClientRef client = 0;
+  OSStatus erc = MIDIClientCreate (CFSTR ("MIDI Client"), midiChanged,
+                                   (__bridge void *)self /*notifyRefCon*/, &client);
+  if (erc != noErr) {
+    NSLog (@"MIDIClientCreate: %u", erc);
+  } else {
+    erc = MIDIInputPortCreate (client, CFSTR ("MIDI Input Port"), readMIDI, nullptr /*refCon*/,
+                               &self->port_);
+    if (erc != noErr) {
+      NSLog (@"MIDIInputPortCreate: %u", erc);
+    }
+  }
+
+  if (erc == noErr) {
+    ItemCount const sources = MIDIGetNumberOfSources ();
+    NSLog (@"There are %u sources", static_cast<unsigned> (sources));
+    if (sources == 0) {
+      NSLog (@"No MIDI source was available");
+      return noErr;
+    }
+
+    self->source_ = MIDIGetSource (0);
+
+    CFStringRef pname = Nil;
+    MIDIObjectGetStringProperty (self->source_, kMIDIPropertyName, &pname);
+    NSLog (@"Connected to: %@", pname);
+    CFRelease (pname);
+
+    erc = MIDIPortConnectSource (self->port_, self->source_, (__bridge void *)self /*connRefCon*/);
+    if (erc != noErr) {
+      NSLog (@"MIDIPortConnectSource: %d", erc);
+    }
+  }
+
+  return erc;
 }
 
 // callback
@@ -252,6 +444,10 @@ static void callback (void *__nullable userData, AudioQueueRef queue, AudioQueue
     }
   }
 
+  if (erc == noErr) {
+    erc = [self startMIDI];
+  }
+
   if (erc != noErr) {
     [self showError:erc];
 #if TARGET_OS_OSX
@@ -261,34 +457,8 @@ static void callback (void *__nullable userData, AudioQueueRef queue, AudioQueue
 #endif
   }
 
-#if 1
-  dispatch_async (dispatch_get_global_queue (QOS_CLASS_USER_INITIATED, 0), ^{
-    MIDIClientRef client = 0;
-    MIDIPortRef port = 0;
-    OSStatus erc = MIDIClientCreate (CFSTR ("MIDI Client"), nullptr /*notify proc*/,
-                                     nullptr /*notifyRefCon*/, &client);
-    NSLog (@"MIDIClientCreate: %u", erc);
-    erc = MIDIInputPortCreate (client, CFSTR ("MIDI Input Port"), ReadMIDIdevice,
-                               nullptr /*refCon*/, &port);
-    NSLog (@"MIDIInputPortCreate: %u", erc);
-
-    ItemCount const sources = MIDIGetNumberOfSources ();
-    NSLog (@"There are %u sources", static_cast<unsigned> (sources));
-    if (sources == 0) {
-      NSLog (@"No MIDI source was available");
-      return;
-    }
-
-    MIDIEndpointRef ep = MIDIGetSource (0);
-
-    CFStringRef pname = Nil;
-    MIDIObjectGetStringProperty (ep, kMIDIPropertyName, &pname);
-    NSLog (@"Connected to: %@", pname);
-    CFRelease (pname);
-
-    erc = MIDIPortConnectSource (port, ep, (__bridge void *)self /*connRefCon*/);
-    NSLog (@"MIDIPortConnectSource: %d", erc);
-  });
+#if 0
+  dispatch_async (dispatch_get_global_queue (QOS_CLASS_USER_INITIATED, 0), ^{});
 #endif
 
 #if TARGET_OS_IOS
